240662242faba20048da1d349d3fe519d9dd3dbf
xmready <xmready@protonmail.com>
2022-09-28 15:45:59 -0700

    fix: .bashrc config

--- setupserv.sh
+++ setupserv.sh
@@ -44,8 +44,8 @@ echo -e "\n$(tput setaf 3)cleaning sources list\n$(tput sgr0)" \
 
 # .bashrc customization
 && echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
-&& echo 'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' >> ~/.bashrc \
-&& echo 'stty -ixon' >> ~/.bashrc \
+&& echo -e "\n"'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' >> ~/.bashrc \
+&& echo -e "\n"'stty -ixon' >> ~/.bashrc \
 && sed -i 's/^HISTSIZE.*/HISTSIZE=10000/' ~/.bashrc \
 && sed -i 's/^HISTFILESIZE.*/HISTFILESIZE=20000/' ~/.bashrc \
 && echo -e "\n$(tput setaf 2)bashrc customized\n$(tput sgr0)" \
