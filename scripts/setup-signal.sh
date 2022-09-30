5f518e110ac7f4d56c27db2b3a7701a6baed3b2c
xmready <xmready@protonmail.com>
2022-09-30 16:43:05 -0700

    refactor: add variables for arch and repo

--- scripts/setup-signal.sh
+++ scripts/setup-signal.sh
@@ -4,14 +4,16 @@
 
 PGPURL=https://updates.signal.org/desktop/apt/keys.asc
 KEYRING=/usr/share/keyrings/signal-desktop-keyring.gpg
-SOURCES=/etc/apt/sources.list.d/signal.list
+ARCH=amd64
+REPOURL=https://updates.signal.org/desktop/apt
 DISTRO=xenial
+SOURCES=/etc/apt/sources.list.d/signal.list
 
 echo -e "\n$(tput setaf 3)adding Signal repo\n$(tput sgr0)" \
 && curl -fL "$PGPURL" \
   | gpg --dearmor \
   | sudo tee "$KEYRING" > /dev/null \
-&& echo "deb [arch=amd64 signed-by=$KEYRING] https://updates.signal.org/desktop/apt $DISTRO main" \
+&& echo "deb [arch=$ARCH signed-by=$KEYRING] $REPOURL $DISTRO main" \
   | sudo tee "$SOURCES" \
 && echo -e "\n$(tput setaf 2)Signal repo added\n$(tput sgr0)" \
 && sleep 3 \
