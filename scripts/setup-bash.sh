2b194bfca8da0a1c826bbe07c070bb12fbfbf80b
xmready <xmready@protonmail.com>
2022-10-01 14:30:03 -0700

    feat: add setup-bash.sh

---
+++ scripts/setup-bash.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# .bashrc customization
+
+echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
+&& echo -e "\n"'stty -ixon' >> ~/.bashrc \
+&& echo \
+  $'\nexport PS1=\'\\n ${debian_chroot:+($debian_chroot)}\[\\033[01;34m\]\W \[\\033[31m\]$(git branch -v 2>/dev/null)\[\\033[00m\]\\n └─🔥 \'' \
+  >> ~/.bashrc \
+&& echo \
+  $'\ntrap \'printf "\\n" 2>/dev/null && echo -e "\\e[1;35m$(date +%T 2>/dev/null)\\e[0;00m"\' DEBUG' \
+  >> ~/.bashrc \
+&& sed -i 's/^HISTSIZE.*/HISTSIZE=10000/' ~/.bashrc \
+&& sed -i 's/^HISTFILESIZE.*/HISTFILESIZE=20000/' ~/.bashrc \
+&& echo -e "\n$(tput setaf 2)bashrc customized\n$(tput sgr0)"
