1e16ff98c5fd96f73fb27a048a08595c519c651d
xmready <xmready@protonmail.com>
2022-10-03 15:09:19 -0700

    fix: update setup-vim.sh URL

--- setup-system.sh
+++ setup-system.sh
@@ -6,7 +6,7 @@
 SETUPAPT=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-apt.sh
 SETUPSIGNAL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-signal.sh
 SETUPTOR=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-tor.sh
-SETUPVIM=https://raw.githubusercontent.com/xmready/vim-config/main/vim-config.sh
+SETUPVIM=https://raw.githubusercontent.com/xmready/vim-config/main/setup-vim.sh
 CLEANAPT=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh
 SETUPCOMMANDS=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-commands.sh
 HARDENNETWORK=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/harden-network.sh
