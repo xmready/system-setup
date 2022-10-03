94e81dd7c744269f22a2083eb624f3cfb348c678
xmready <xmready@protonmail.com>
2022-10-03 12:44:26 -0700

    refactor: VIMRCROOT variable

--- scripts/setup-server-vim.sh
+++ scripts/setup-server-vim.sh
@@ -2,9 +2,11 @@
 
 # vim setup for a server
 
+VIMRCROOT=https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc-root
+
 echo -e "\n$(tput setaf 3)configuring Vim\n$(tput sgr0)" \
-&& curl -fLo ~/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc-root \
-&& sudo curl -fLo /root/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc-root \
+&& curl -fLo ~/.vimrc "$VIMRCROOT" \
+&& sudo curl -fLo /root/.vimrc "$VIMRCROOT" \
 && sleep 3 \
 && sudo -v \
 && echo -e "\n$(tput setaf 2)Vim configured\n$(tput sgr0)"
