6eb13e211bd7361674755a28303aa7b1b1f0259b
xmready <xmready@protonmail.com>
2022-10-03 15:13:34 -0700

    fix: timestamps and ufw config

--- scripts/harden-network.sh
+++ scripts/harden-network.sh
@@ -7,12 +7,14 @@ CONFIGURL=https://github.com/xmready/system-setup/blob/main/configs/90-tcp_times
 
 echo -e "\n$(tput setaf 3)disabling tcp timestamps\n$(tput sgr0)" \
 && sudo curl -fLo "$TIMESTAMPS" "$CONFIGURL" \
-&& sudo sysctl -a | grep timestamps \
+&& sudo sysctl -q --system 2> /dev/null \
+&& sudo sysctl -a 2> /dev/null | grep timestamps \
 && echo -e "\n$(tput setaf 2)tcp timestamps disabled\n$(tput sgr0)" \
 && sleep 3 \
 && echo -e "\n$(tput setaf 3)configuring firewall\n$(tput sgr0)" \
-&& sudo ufw enable \
 && sudo ufw default deny incoming \
+&& sudo ufw default allow outgoing \
+&& sudo ufw --force enable \
 && sudo ufw status verbose \
 && sudo -v \
 && echo -e "\n$(tput setaf 2)firewall configured\n$(tput sgr0)"
