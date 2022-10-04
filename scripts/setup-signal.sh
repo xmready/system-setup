69800f2620fef23f4c4570d690d257cd65449e0e
xmready <xmready@protonmail.com>
2022-10-03 17:02:49 -0700

    fix: remove architecture in tor and signal setup

--- scripts/setup-signal.sh
+++ scripts/setup-signal.sh
@@ -4,7 +4,6 @@
 
 PGPURL=https://updates.signal.org/desktop/apt/keys.asc
 KEYRING=/usr/share/keyrings/signal-desktop-keyring.gpg
-ARCH=amd64
 REPOURL=https://updates.signal.org/desktop/apt
 DISTRO=xenial
 SOURCES=/etc/apt/sources.list.d/signal.list
@@ -13,7 +12,7 @@ echo -e "\n$(tput setaf 3)adding Signal repo\n$(tput sgr0)" \
 && curl -fL "$PGPURL" \
   | gpg --dearmor \
   | sudo tee "$KEYRING" > /dev/null \
-&& echo "deb [arch=$ARCH signed-by=$KEYRING] $REPOURL $DISTRO main" \
+&& echo "deb [signed-by=$KEYRING] $REPOURL $DISTRO main" \
   | sudo tee "$SOURCES" \
 && echo -e "\n$(tput setaf 2)Signal repo added\n$(tput sgr0)" \
 && sleep 3 \
