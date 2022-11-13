f8372b7d71cbcc773b9981c6b03b0001b4077bcd
Devan Mardyks <xmready@protonmail.com>
2022-11-12 18:33:13 -0800

    feat: add bash command history settings

--- scripts/setup-bash.sh
+++ scripts/setup-bash.sh
@@ -4,6 +4,8 @@
 
 echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
 && echo -e "\n"'stty -ixon' >> ~/.bashrc \
+&& echo -e "\n"'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' \
+  >> ~/.bashrc \
 && echo \
   $'\nexport PS1=\'\\n ${debian_chroot:+($debian_chroot)}\[\\033[01;35m\]\t \[\\033[34m\]\W \[\\033[31m\]$(git branch -v 2>/dev/null)\[\\033[00m\]\\n └─🔥 \'' \
   >> ~/.bashrc \
