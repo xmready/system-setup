af27054f592024dd4f97b87d43d59c4b4302f364
Devan Mardyks <xmready@protonmail.com>
2022-11-13 23:37:00 -0800

    fix: remove lines and fix rclone file name

--- scripts/autoupgrade.sh
+++ scripts/autoupgrade.sh
@@ -5,16 +5,10 @@
 echo -e "\n$(tput setaf 3)performing upgrade\n$(tput sgr0)" \
 && sudo apt update \
 && sudo apt -y upgrade \
-
-# rclone install latest version
 && curl -fLo /tmp/rclone.deb https://downloads.rclone.org/rclone-current-linux-amd64.deb \
 && sudo apt install /tmp/rclone.deb \
-&& sudo rm /tmp/rclone-current-linux-amd64.deb \
-
-# apt remove and clean
+&& sudo rm /tmp/rclone.deb \
 && sudo apt -y autoremove \
 && sudo apt autoclean \
-
-# flatpak update
 && flatpak update -y \
 && echo -e "\n$(tput setaf 2)upgrade complete\n$(tput sgr0)"
