0085b2270c0668e8eeb3e0b2cee2945eaafb0036
Devan Mardyks <xmready@protonmail.com>
2022-11-14 11:19:30 -0800

    fix: remove trap and escape time special char bash

--- scripts/setup-bash.sh
+++ scripts/setup-bash.sh
@@ -7,10 +7,7 @@ echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
 && echo -e "\n"'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' \
   >> ~/.bashrc \
 && echo \
-  $'\nexport PS1=\'\\n ${debian_chroot:+($debian_chroot)}\[\\033[01;35m\]\t \[\\033[34m\]\W \[\\033[31m\]$(git branch -v 2>/dev/null)\[\\033[00m\]\\n └─🔥 \'' \
-  >> ~/.bashrc \
-&& echo \
-  $'\ntrap \'printf "\\n" 2>/dev/null && echo -e "\\e[1;35m$(date +%T 2>/dev/null)\\e[0;00m"\' DEBUG' \
+  $'\nexport PS1=\'\\n ${debian_chroot:+($debian_chroot)}\[\\033[01;35m\]\\t \[\\033[34m\]\W \[\\033[31m\]$(git branch -v 2>/dev/null)\[\\033[00m\]\\n └─🔥 \'' \
   >> ~/.bashrc \
 && sed -i 's/^HISTSIZE.*/HISTSIZE=10000/' ~/.bashrc \
 && sed -i 's/^HISTFILESIZE.*/HISTFILESIZE=20000/' ~/.bashrc \
