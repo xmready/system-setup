3bf6e873ec1b3294ab4523575eaf5fa90bfc75a5
Devan Mardyks <xmready@protonmail.com>
2022-11-29 10:16:19 -0800

    fix: remove keepassxc

--- scripts/setup-apt.sh
+++ scripts/setup-apt.sh
@@ -20,8 +20,8 @@ echo -e "\n$(tput setaf 3)cleaning sources list\n$(tput sgr0)" \
 && echo -e "\n$(tput setaf 3)installing packages\n$(tput sgr0)" \
 && sudo apt install -y \
   bash-completion build-essential checkinstall curl figlet \
-  flatpak git gnome-software-plugin-flatpak gnupg keepassxc \
-  lm-sensors lxc neofetch nmap qrencode rsync ssh-audit ufw \
+  flatpak git gnome-software-plugin-flatpak gnupg lm-sensors \
+  lxc neofetch nmap qrencode rsync ssh-audit ufw \
   zfsutils-linux xclip \
 && echo -e "\n$(tput setaf 2)packages installed\n$(tput sgr0)" \
 && echo -e "\n$(tput setaf 3)installing rclone\n$(tput sgr0)" \
