aa366571e9cff5d589bf4546d78580bac66e6b9b
Devan Mardyks <xmready@protonmail.com>
2022-11-30 15:27:48 -0800

    fix: remove signal setup

--- setup-system.sh
+++ setup-system.sh
@@ -10,7 +10,6 @@
 #   wget --quiet -O- https://raw.githubusercontent.com/xmready/system-setup/main/setup-system.sh | bash -
 
 SETUPAPPS=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-apps.sh
-SETUPSIGNAL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-signal.sh
 SETUPTOR=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-tor.sh
 SETUPVIM=https://raw.githubusercontent.com/xmready/vim-config/main/setup-vim.sh
 CLEANAPT=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh
@@ -19,8 +18,6 @@ HARDENNETWORK=https://raw.githubusercontent.com/xmready/system-setup/main/script
 SETUPBASH=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-bash.sh
 
 wget --quiet -O- "$SETUPAPPS" | bash - \
-&& sleep 3 \
-&& curl -fsSL "$SETUPSIGNAL" | bash - \
 && sleep 3 \
 && curl -fsSL "$SETUPTOR" | bash - \
 && sleep 3 \
