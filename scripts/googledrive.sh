e4f11f1d176ffd11ffe349606d3fe34c8c1dc502
Devan Mardyks <xmready@protonmail.com>
2022-11-30 22:39:37 -0800

    fix: permissions rclone mount

--- scripts/googledrive.sh
+++ scripts/googledrive.sh
@@ -2,5 +2,5 @@
 #mount google drive to local directory
 
 sleep 10 \
-&& rclone mount gdrive: ~/gdrive --daemon --vfs-cache-mode full --dir-perms 755 --file-perms 640 \
+&& rclone mount gdrive: ~/gdrive --daemon --vfs-cache-mode full \
 && rclone mount vault: ~/vault --daemon --vfs-cache-mode full --dir-perms 700 --file-perms 600
