0e319716d6b1ea53abf58a99ddb2eb5f3d9655c6
Devan Mardyks <xmready@protonmail.com>
2022-12-04 21:51:21 -0800

    fix: change bash prompt to show full working path

--- scripts/setup-bash.sh
+++ scripts/setup-bash.sh
@@ -7,7 +7,7 @@ echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
 && echo -e "\n"'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' \
   >> ~/.bashrc \
 && echo \
-  $'\nexport PS1=\'\\n ${debian_chroot:+($debian_chroot)}\[\\033[01;35m\]\\t \[\\033[34m\]\W \[\\033[31m\]$(git branch -v 2>/dev/null)\[\\033[00m\]\\n └─🔥 \'' \
+  $'\nexport PS1=\'\\n ${debian_chroot:+($debian_chroot)}\[\\033[01;35m\]\\t \[\\033[34m\]\w \[\\033[31m\]$(git branch -v 2>/dev/null)\[\\033[00m\]\\n └─🔥 \'' \
   >> ~/.bashrc \
 && sed -i 's/^HISTSIZE.*/HISTSIZE=10000/' ~/.bashrc \
 && sed -i 's/^HISTFILESIZE.*/HISTFILESIZE=20000/' ~/.bashrc \
