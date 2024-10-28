791b05c9bb267fe0fb3ef0ef845354cb218aec6d
Devan Mardyks <xmready@protonmail.com>
2024-10-28 16:43:23 -0700

    feat: add nvm installation and config

--- setup-system.sh
+++ setup-system.sh
@@ -7,29 +7,32 @@
 #   A script to setup a new debian based desktop/laptop
 #
 # Non-root usage:
-#   wget --quiet -O- https://raw.githubusercontent.com/xmready/system-setup/main/setup-system.sh | bash -
+#   wget -O- https://raw.githubusercontent.com/xmready/system-setup/main/setup-system.sh | bash -
 
-SETUPAPPS=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-apps.sh
-SETUPTOR=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-tor.sh
-SETUPVIM=https://raw.githubusercontent.com/xmready/vim-config/main/setup-vim.sh
-CLEANAPT=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh
-SETUPCOMMANDS=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-commands.sh
-HARDENNETWORK=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/harden-network.sh
-SETUPSHELL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-shell.sh
+SETUP_APPS=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-apps.sh
+SETUP_TOR=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-tor.sh
+SETUP_NVM=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-nvm.sh
+SETUP_VIM=https://raw.githubusercontent.com/xmready/vim-config/main/setup-vim.sh
+CLEAN_APT=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh
+SETUP_COMMANDS=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-commands.sh
+HARDEN_NETWORK=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/harden-network.sh
+SETUP_SHELL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-shell.sh
 
-wget --quiet -O- "$SETUPAPPS" | bash - \
+wget --quiet -O- "$SETUP_APPS" | bash - \
 && sleep 3 \
-&& curl -fsSL "$SETUPTOR" | bash - \
+&& curl -fsSL "$SETUP_TOR" | bash - \
 && sleep 3 \
-&& curl -fsSL "$SETUPVIM" | bash - \
+&& curl -fsSL "$SETUP_NVM" | bash - \
 && sleep 3 \
-&& curl -fsSL "$CLEANAPT" | bash - \
+&& curl -fsSL "$SETUP_VIM" | bash - \
 && sleep 3 \
-&& curl -fsSL "$SETUPCOMMANDS" | bash - \
+&& curl -fsSL "$CLEAN_APT" | bash - \
 && sleep 3 \
-&& curl -fsSL "$HARDENNETWORK" | bash - \
+&& curl -fsSL "$SETUP_COMMANDS" | bash - \
 && sleep 3 \
-&& curl -fsSL "$SETUPSHELL" | bash - \
+&& curl -fsSL "$HARDEN_NETWORK" | bash - \
+&& sleep 3 \
+&& curl -fsSL "$SETUP_SHELL" | bash - \
 && sleep 3 \
 && echo -e \
   "\n$(tput setaf 1)$(tput bold)SYSTEM WILL REBOOT IN 60 SECONDS\n$(tput sgr0)$(tput bel)" \
