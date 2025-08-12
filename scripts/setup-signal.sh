cd7f0bb63e839fb970c45bda4b6e19fa4243a5b4
xmready <77082140+xmready@users.noreply.github.com>
2025-08-11 19:13:29 -0700

    Update setup-signal.sh

--- scripts/setup-signal.sh
+++ scripts/setup-signal.sh
@@ -11,17 +11,17 @@
 
 PGP_KEYS_URL=https://updates.signal.org/desktop/apt/keys.asc
 KEYRING_PATH=/usr/share/keyrings/signal-desktop-keyring.gpg
+SOURCES_URL=https://raw.githubusercontent.com/xmready/system-setup/refs/heads/main/configs/signal.sources
+SOURCES_PATH=/etc/apt/sources.list.d/signal.sources
 ARCH="$(dpkg --print-architecture)"
-DEB_REPO_URL=https://updates.signal.org/desktop/apt
-DISTRO=xenial
-SOURCES_PATH=/etc/apt/sources.list.d/signal.list
 
 echo -e "\n$(tput setaf 3)adding Signal repo\n$(tput sgr0)" \
 && curl -fL "$PGP_KEYS_URL" \
   | gpg --dearmor \
   | sudo tee "$KEYRING_PATH" > /dev/null \
-&& echo "deb [arch=$ARCH signed-by=$KEYRING_PATH] $DEB_REPO_URL $DISTRO main" \
-  | sudo tee "$SOURCES_PATH" \
+&& curl -fLo "$SOURCES_URL" \
+  | sed s/architecture/"$ARCH"/ \
+  | sudo tee "$SOURCES_PATH" > /dev/null \
 && echo -e "\n$(tput setaf 2)Signal repo added\n$(tput sgr0)" \
 && sleep 3 \
 && echo -e "\n$(tput setaf 3)installing Signal\n$(tput sgr0)" \
