e4b03d3e18c166dafa44574c7c6b962dc77a8d5f
xmready <77082140+xmready@users.noreply.github.com>
2025-08-11 20:56:57 -0700

    Update harden-network.sh

--- scripts/harden-network.sh
+++ scripts/harden-network.sh
@@ -2,11 +2,11 @@
 
 # harden network with firewall and timestamps
 
-TIMESTAMPS=/etc/sysctl.d/90-tcp_timestamps.conf
-STAMPSURL=https://raw.githubusercontent.com/xmready/system-setup/main/configs/90-tcp_timestamps.conf
+TIMESTAMPS_PATH=/etc/sysctl.d/90-tcp_timestamps.conf
+TIMESTAMPS_URL=https://raw.githubusercontent.com/xmready/system-setup/main/configs/90-tcp_timestamps.conf
 
 echo -e "\n$(tput setaf 3)disabling tcp timestamps\n$(tput sgr0)" \
-&& sudo curl -fLo "$TIMESTAMPS" "$STAMPSURL" \
+&& sudo curl -fLo "$TIMESTAMPS_PATH" "$TIMESTAMPS_URL" \
 && sudo sysctl -q --system 2> /dev/null \
 && sudo sysctl -a 2> /dev/null | grep timestamps \
 && echo -e "\n$(tput setaf 2)tcp timestamps disabled\n$(tput sgr0)" \
